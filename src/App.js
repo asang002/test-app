bcc19c92f2f2a65ec27c9eb528f2008cdee9681d
asangroula <asangroula@hubspot.com>
2024-08-03 11:40:34 -0500

    updates here

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-   <iframe src="https://asangroulatest.github.io/test-app/" title="test-this" height="1000px" width="1000px"/>
+
+    <div className="App" style={{padding: '8px',position: 'absolute', bottom: '0px', right: '0px'}}>
+   <iframe src="https://asangroulatest.github.io/test-app/" title="test-this" height="300px" width="400px"/>
     </div>
   );
 }
